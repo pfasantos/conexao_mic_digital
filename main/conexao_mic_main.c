129dabd58cac0b931674f5f23356e65b357905ed
pfasantos <pedro.felp2203@gmail.com>
2025-09-26 10:45:43 -0300

    i2s_pdm completo e ajustes cmake

--- main/conexao_mic_main.c
+++ main/conexao_mic_main.c
@@ -16,17 +16,22 @@
 #include "protocol_examples_common.h"
 
 //tags
+/*
 static const char *I2S_TAG = "I2S";
 static const char *UDP_TAG = "UDP";
 static const char *MAIN_TAG = "MAIN";
+*/
 
 //macros
-#define DMA_BUF_NUM 
-#define DMA_BUF_SIZE
+#define DMA_BUF_NUM 16 //numero de buffers do dma
+#define DMA_BUF_SIZE 1024 //tamanho de cada buffer
+#define BUF_SIZE 4096 // tamanho de buffer de recepcao de dados
 
-/****** inicio da configuracao i2s *******/
 
-i2s_chan_handle_t rx_handle; //handler pra canal i2s
+//configuracao i2s
+void i2s_pdm()
+{
+    i2s_chan_handle_t rx_handle; //handler pra canal i2s
 
 i2s_chan_config_t chan_cfg =
 {
@@ -39,7 +44,7 @@ i2s_chan_config_t chan_cfg =
 
 i2s_new_channel(&chan_cfg, NULL, &rx_handle); //criar canal i2s
 
-i2s_pdm_rx_gpio_config_t pdm_rx_cfg = 
+i2s_pdm_rx_config_t pdm_rx_cfg = 
 {
     .clk_cfg = I2S_PDM_RX_CLK_DEFAULT_CONFIG(44100),
     .slot_cfg = I2S_PDM_RX_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
@@ -55,4 +60,4 @@ i2s_pdm_rx_gpio_config_t pdm_rx_cfg =
 };
 
 i2s_channel_init_pdm_rx_mode(rx_handle, &pdm_rx_cfg);
-
+}
